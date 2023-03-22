067e0e997bb7666567eee773689cf545e3cb3e37
Yury Olennikov <OlennikovYV@mail.ru>
2023-03-22 12:33:15 +0500

    Быстрая сортировка - изменения. значения равные pivot сразу попадают в отсортированный массив.

--- src/quick-sort/index.js
+++ src/quick-sort/index.js
@@ -4,23 +4,21 @@ function quickSort(array) {
   let pivot = array[0];
   const left = [];
   const right = [];
+  const equal = [];
 
   if (array.length < 2) return array;
 
-  for (let i = 1; i < array.length; i += 1) {
-    const currentElement = array[i];
-
-    if (pivot > array[i]) {
-      left.push(currentElement);
-    } else {
-      right.push(currentElement);
-    }
+  for (let currentElement of array) {
+    if (pivot > currentElement) left.push(currentElement);
+    if (pivot < currentElement) right.push(currentElement);
+    if (pivot === currentElement) equal.push(currentElement);
   }
 
-  return quickSort(left).concat(pivot, quickSort(right));
+  return quickSort(left).concat(equal, quickSort(right));
 }
 
 console.log(quickSort([])); // []
 console.log(quickSort([3])); // [3]
 console.log(quickSort([3, 4, 6, 2, 1])); // [1, 2, 3, 4, 6]
 console.log(quickSort([3, 2, 5, 2, 6])); // [2, 2, 3, 5, 6]
+console.log(quickSort([3, 2, 5, 2, 2])); // [2, 2, 2, 3, 5]
